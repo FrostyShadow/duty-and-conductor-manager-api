28cd108f9e2fdffceea0e4f07d70f87637d1c3ff
Filip Kosmala <kosmala.filip@gmail.com>
2022-03-16 16:09:28 +0100

    Add documentation to the rest of the API

--- Controllers/LineController.cs
+++ Controllers/LineController.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using DutyAndConductorManager.Api.Services;
 using DutyAndConductorManager.Api.Models;
+using Swashbuckle.AspNetCore.Annotations;
+using DutyAndConductorManager.Api.Entities;
+using DutyAndConductorManager.Api.Helpers;
 
 namespace DutyAndConductorManager.Api.Controllers;
 
@@ -17,8 +20,9 @@ public class LineController : ControllerBase
     }
 
     [HttpGet]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    public async Task<IActionResult> GetById([FromQuery] int id)
+    [SwaggerOperation("Gets line by it's ID")]
+    [ProducesResponseType(typeof(Line), 200)]
+    public async Task<IActionResult> GetById([FromQuery, SwaggerParameter("Line ID")] int id)
     {
         var response = await _lineService.GetById(id);
 
@@ -26,7 +30,8 @@ public class LineController : ControllerBase
     }
 
     [HttpGet]
-    [ProducesResponseType(StatusCodes.Status200OK)]
+    [SwaggerOperation("Gets list of all lines")]
+    [ProducesResponseType(typeof(IList<Line>), 200)]
     public async Task<IActionResult> GetAll()
     {
         var response = await _lineService.GetAll();
@@ -35,9 +40,10 @@ public class LineController : ControllerBase
     }
 
     [HttpPost]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<IActionResult> AddLine([FromBody] AddLineRequest model)
+    [SwaggerOperation("Creates new line")]
+    [ProducesResponseType(typeof(bool), 200)]
+    [ProducesResponseType(typeof(ErrorResponse), 400)]
+    public async Task<IActionResult> AddLine([FromBody, SwaggerRequestBody("Line data")] AddLineRequest model)
     {
         var response = await _lineService.AddLine(model);
 
@@ -48,9 +54,10 @@ public class LineController : ControllerBase
     }
 
     [HttpPost]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<IActionResult> EditLine([FromBody] EditLineRequest model)
+    [SwaggerOperation("Edits existing line")]
+    [ProducesResponseType(typeof(bool), 200)]
+    [ProducesResponseType(typeof(ErrorResponse), 400)]
+    public async Task<IActionResult> EditLine([FromBody, SwaggerRequestBody("Line data")] EditLineRequest model)
     {
         var response = await _lineService.EditLine(model);
 
@@ -61,9 +68,10 @@ public class LineController : ControllerBase
     }
 
     [HttpPost]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<IActionResult> DeleteLine([FromBody] DeleteLineRequest model)
+    [SwaggerOperation("Deletes existing line")]
+    [ProducesResponseType(typeof(bool), 200)]
+    [ProducesResponseType(typeof(ErrorResponse), 400)]
+    public async Task<IActionResult> DeleteLine([FromBody, SwaggerRequestBody("Line ID")] DeleteLineRequest model)
     {
         var response = await _lineService.DeleteLine(model);
 
